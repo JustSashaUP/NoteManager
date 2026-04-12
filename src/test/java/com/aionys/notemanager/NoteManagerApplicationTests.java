146804568305391d5a94679d2f9e014a8863b274
Oleksandr Savchenko <alexandr.savchenko04@gmail.com>
2026-04-12 20:30:24 +0300

    test: configure SpringBootTest to use PostgreSQL Testcontainers

--- src/test/java/com/aionys/notemanager/NoteManagerApplicationTests.java
+++ src/test/java/com/aionys/notemanager/NoteManagerApplicationTests.java
@@ -2,12 +2,40 @@ package com.aionys.notemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.TimeZone;
+
+@Testcontainers
 @SpringBootTest
 class NoteManagerApplicationTests {
+
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14")
+            .withDatabaseName("note")
+            .withUsername("root")
+            .withPassword("root")
+            .withEnv("TZ", "UTC");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+        registry.add("spring.jpa.properties.hibernate.jdbc.time_zone", () -> "UTC");
+    }
 
     @Test
     void contextLoads() {
     }
-
 }
